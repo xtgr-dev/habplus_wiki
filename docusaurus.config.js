c2a105a3c82e93b389819b71fc977c3dea656d12
xtgr <252477181+xtgr-dev@users.noreply.github.com>
2026-01-01 18:30:37 +0000

    GitHub Pages config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,16 +20,16 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://xtgr-dev.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/habplus_wiki/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'xtgr-dev', // Usually your GitHub org/user name.
+  projectName: 'habplus_wiki', // Usually your repo name.
+  trailingSlash: false,
   onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
